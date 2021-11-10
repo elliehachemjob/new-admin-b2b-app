35ca13bfc78a699f9a35fc2207d437761f09f1c4
elliehachemjob <elliehachemjob@gmail.com>
2021-11-10 16:02:35 +0200

    --

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import "./sidebar.css";
+import LineStyleIcon from "@mui/icons-material/LineStyle";
 
 interface Props {}
 
 export const SideBar: React.FC<Props> = (props: any): any => {
-  return <div className="sidebar">sidebar</div>;
+  return (
+    <div className="sidebar">
+      <div className="sidebarWrapper">
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Dashboard</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem">
+              <LineStyleIcon />
+              Home
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 };
