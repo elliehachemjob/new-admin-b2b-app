203372e530fcaa350115728a06666e8572998732
elliehachemjob <elliehachemjob@gmail.com>
2021-11-10 17:04:26 +0200

    --

--- src/components/featuredInfo/FeaturedInfo.tsx
+++ src/components/featuredInfo/FeaturedInfo.tsx
@@ -23,7 +23,7 @@ export const FeaturedInfo: React.FC<Props> = (props: any): any => {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">$4,415$</span>
           <span className="featuredMoneyRate">
-            -11.4 <ArrowDownwardIcon />
+            -11.4 <ArrowDownwardIcon className="featuredIcon negative" />
           </span>
         </div>
         <span className="featuredSub">Compare to last month</span>
@@ -33,7 +33,7 @@ export const FeaturedInfo: React.FC<Props> = (props: any): any => {
         <div className="featuredMoneyContainer">
           <span className="featuredMoney">$2,225$</span>
           <span className="featuredMoneyRate">
-            +2.4 <ArrowUpwardIcon />
+            +2.4 <ArrowUpwardIcon className="featuredIcon" />
           </span>
         </div>
         <span className="featuredSub">Compare to last month</span>
