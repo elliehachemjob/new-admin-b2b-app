41aeec0902c2df361540122abec3cad6c20b49a4
elliehachemjob <elliehachemjob@gmail.com>
2021-11-10 16:30:17 +0200

    --

--- src/components/sidebar/SideBar.tsx
+++ src/components/sidebar/SideBar.tsx
@@ -3,6 +3,15 @@ import "./sidebar.css";
 import LineStyleIcon from "@mui/icons-material/LineStyle";
 import TimelineIcon from "@mui/icons-material/Timeline";
 import TrendingUpIcon from "@mui/icons-material/TrendingUp";
+import PermIdentityIcon from "@mui/icons-material/PermIdentity";
+import StoreIcon from "@mui/icons-material/Store";
+import AttachMoneyIcon from "@mui/icons-material/AttachMoney";
+import BarChartIcon from "@mui/icons-material/BarChart";
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import DynamicFeedIcon from "@mui/icons-material/DynamicFeed";
+import ChatBubbleOutlineIcon from "@mui/icons-material/ChatBubbleOutline";
+import WorkOutlineIcon from "@mui/icons-material/WorkOutline";
+import ReportIcon from "@mui/icons-material/Report";
 
 interface Props {}
 
@@ -13,17 +22,68 @@ export const SideBar: React.FC<Props> = (props: any): any => {
         <div className="sidebarMenu">
           <h3 className="sidebarTitle">Dashboard</h3>
           <ul className="sidebarList">
-            <li className="sidebarListItem active">
-              <LineStyleIcon />
+            <li className="sidebarListItem ">
+              <LineStyleIcon className="sidebarIcon" />
               Home
             </li>
             <li className="sidebarListItem">
-              <TimelineIcon />
+              <TimelineIcon className="sidebarIcon" />
               Analytics
             </li>
             <li className="sidebarListItem">
-              <TrendingUpIcon />
+              <TrendingUpIcon className="sidebarIcon" />
               Sales
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Quick Menu</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <PermIdentityIcon className="sidebarIcon" />
+              Users
+            </li>
+            <li className="sidebarListItem">
+              <StoreIcon className="sidebarIcon" />
+              Products
+            </li>
+            <li className="sidebarListItem">
+              <AttachMoneyIcon className="sidebarIcon" />
+              Transactions
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Notifications</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <BarChartIcon className="sidebarIcon" />
+              Reports
+            </li>
+            <li className="sidebarListItem">
+              <MailOutlineIcon className="sidebarIcon" />
+              Mail
+            </li>
+            <li className="sidebarListItem">
+              <DynamicFeedIcon className="sidebarIcon" />
+              Feedback
+            </li>
+          </ul>
+        </div>
+        <div className="sidebarMenu">
+          <h3 className="sidebarTitle">Staff</h3>
+          <ul className="sidebarList">
+            <li className="sidebarListItem ">
+              <ChatBubbleOutlineIcon className="sidebarIcon" />
+              Message
+            </li>
+            <li className="sidebarListItem">
+              <WorkOutlineIcon className="sidebarIcon" />
+              Manage
+            </li>
+            <li className="sidebarListItem">
+              <ReportIcon className="sidebarIcon" />
+              reports
             </li>
           </ul>
         </div>
