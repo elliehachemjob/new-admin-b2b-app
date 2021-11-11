bb02d6954b22d5585452451a8374cd1d8d73c0ab
elliehachemjob <elliehachemjob@gmail.com>
2021-11-11 15:40:23 +0200

    tt

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserList } from "./pages/userList/UserList";
 import { User } from "./pages/user/User";
 import { NewUser } from "./pages/newUser/NewUser";
+import { ProductList } from "./pages/productList/ProductList";
 
 import "./app.css";
 
@@ -19,7 +20,10 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/users" element={<UserList />} />
           <Route path="/user:userId" element={<User />} />
-          <Route path="/newuser" element={<NewUser />} />
+          <Route path="/newUser" element={<NewUser />} />
+          <Route path="/products" element={<ProductList />} />
+          <Route path="/product:productsId" element={<User />} />
+          <Route path="/newProduct" element={<NewUser />} />=
         </Routes>
       </div>
     </Router>
