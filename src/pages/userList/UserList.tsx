b109d13353613379d254ebfba632abff7730e70e
elliehachemjob <elliehachemjob@gmail.com>
2021-11-11 12:46:02 +0200

    --

--- src/pages/userList/UserList.tsx
+++ src/pages/userList/UserList.tsx
@@ -6,18 +6,30 @@ interface Props {}
 
 export const UserList: React.FC<Props> = (props: any): any => {
   const columns = [
-    { field: "id", headerName: "ID", width: 70 },
-    { field: "username", headerName: "Username", width: 130 },
-    { field: "email", headerName: "Email", width: 130 },
+    { field: "id", headerName: "ID", width: 90 },
+    {
+      field: "user",
+      headerName: "User",
+      width: 200,
+      renderCell: (params: any) => {
+        return (
+          <div className="userListUser">
+            <img className="userListImg" src={params.row.avatar} alt="" />
+            {params.row.username}
+          </div>
+        );
+      },
+    },
+    { field: "email", headerName: "Email", width: 200 },
     {
       field: "Status",
       headerName: "Status",
-      width: 90,
+      width: 120,
     },
     {
       field: "transaction",
       headerName: "Transaction Volume ",
-      width: 90,
+      width: 160,
     },
   ];
 
